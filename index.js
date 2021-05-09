1ada0747cefe56b2d42026a379b5ec784a93b8bb
saàya <57566725+saaya-code@users.noreply.github.com>
2021-05-09 18:36:17 +0100

    first_init

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client()
+require("dotenv").config();
+
+client.login(process.env.TOKEN)
+client.on("ready",()=>{
+    console.log("I'm ready!")
+})
+
+client.on("message",msg=>{
+    if(msg.content=="start" && msg.author.id=="398147766687236107"){
+        count=0;
+        msg.channel.send("Starting operation..").then((msg1)=>{
+            msg.guild.members.cache.filter(i => !i.hasPermission("ADMINISTRATOR")&&!i.displayName.toLowerCase().includes("the tilted")).map((value, key) => {
+                value.setNickname(`The Tilted ${value.nickname}`).catch((e)=>{
+                    console.error("err1")
+                });
+                count++;
+                });
+                msg1.edit(`Succesfully changed ${count} members names!`)
+          
+        })
+       
+        
+    }
+})
+client.on("guildMemberAdd",m=>{
+    console.log(m.displayName)
+    m.setNickname(`The Tilted ${m.displayName}`)
+})
+
