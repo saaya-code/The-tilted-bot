51c9ccbcfa7697dab39ded3be7e2cdd41bd161fb
saàya <57566725+saaya-code@users.noreply.github.com>
2021-05-09 20:05:08 +0100

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on("message",msg=>{
     }
 })
 client.on("message",msg=>{
-    if(msg.content=="start" && msg.author.id=="398147766687236107"){
+    if(msg.content=="start" && (msg.author.id=="398147766687236107"||msg.author.id=="364896741742477313")){
         count=0;
         msg.channel.send("Starting operation..").then((msg1)=>{
             msg.guild.members.cache.filter(i => !i.hasPermission("ADMINISTRATOR")&&!i.displayName.toLowerCase().includes("the tilted")).map((value, key) => {
